f32848c18a13c0402a81548af30588d85f1552ac
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-11-24 01:39:39 +0600

    added create user, user login, logout, user state check form firebase inside context

---
+++ src/context/UserValidation.js
@@ -0,0 +1,57 @@
+import React, { createContext, useEffect, useState } from 'react';
+import app from '../Firebase/firebase.config';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth'
+
+const UserContext = createContext();
+const auth = getAuth(app);
+
+const UserValidation = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loader, setLoader] = useState(true);
+
+    // create a new user via firebase auth system
+    const createNewUser = (email, password) => {
+        setLoader(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const signIn = (email , password) => {
+        setLoader(true)
+        return signInWithEmailAndPassword(auth , email , password)
+    }
+
+    const logOut = () => {
+        return signOut(auth)
+    }
+
+    // observing user
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            if (user === null) {
+                console.log(`from Auth state, observing user!`)
+                setUser(currentUser);
+                setLoader(false)
+            }
+        })
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+
+    // object to share with entire app for auth related executions
+    const fireAuthValue = {
+        user,
+        loader,
+        createNewUser,
+        signIn,
+        logOut
+    }
+
+    return (
+        <UserContext.Provider value={fireAuthValue}>
+            {children}
+        </UserContext.Provider>
+    );
+};
+
+export default UserValidation;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import UserValidation from './context/UserValidation';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <UserValidation>
+      <App />
+    </UserValidation>
   </React.StrictMode>
 );
 
