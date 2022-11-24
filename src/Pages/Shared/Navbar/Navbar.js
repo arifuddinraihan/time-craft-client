7018663e7d78c060011a6867bfd3f21a9709e5cd
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-11-24 21:13:44 +0600

    navbar all set

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -3,9 +3,12 @@ import { Link, NavLink, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
     const navMenu = <>
-        <NavLink className="btn btn-ghost" to={'/dashboard'}>Dashboard</NavLink>
-        <NavLink className="btn btn-ghost">Blog</NavLink>
-        <NavLink className="btn btn-ghost">Login</NavLink>
+        <NavLink className={({isActive}) => isActive ? "btn btn-xs sm:btn-sm md:btn-md btn-outline" : "btn btn-ghost btn-xs sm:btn-sm md:btn-md"} 
+        to={'/dashboard'}>Dashboard</NavLink>
+        <NavLink className={({isActive}) => isActive ? "btn btn-xs sm:btn-sm md:btn-md btn-outline" : "btn btn-ghost btn-xs sm:btn-sm md:btn-md"} 
+        to={'/blog'}>Blog</NavLink>
+        <NavLink className={({isActive}) => isActive ? "btn btn-xs sm:btn-sm md:btn-md btn-outline" : "btn btn-ghost btn-xs sm:btn-sm md:btn-md"}
+        to={'/login'}>Login</NavLink>
     </>
     // location tracker for conditional modal button render in navbar
     const location = useLocation();
@@ -16,14 +19,14 @@ const Navbar = () => {
             <div className="navbar container mx-auto">
                 <div className="navbar-start">
                     {
-                        (dashboardPath === '/dashboard') && <label htmlFor="dashboard-panel-drawer" className="btn btn-md btn-ghost drawer-button lg:hidden">
-                              <svg className="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z"/></svg>
+                        dashboardPath.includes('dashboard') && <label htmlFor="dashboard-panel-drawer" className="btn btn-md btn-ghost drawer-button lg:hidden">
+                            <svg className="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" /></svg>
                         </label>
                     }
                     <Link to={'/'} className="btn btn-ghost normal-case text-xl">daisyUI</Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal p-0">
+                    <ul className="menu menu-horizontal p-0 tabs gap-2">
                         {
                             navMenu
                         }
@@ -36,11 +39,11 @@ const Navbar = () => {
                                 <img src="https://placeimg.com/80/80/people" />
                             </div>
                         </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 bg-opacity-90 rounded-box w-52">
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 bg-opacity-90 rounded-box w-52 gap-2">
                             {
                                 navMenu
                             }
-                            <li className="btn btn-ghost">Logout</li>
+                            <li className="btn btn-xs sm:btn-sm md:btn-md btn-ghost">Logout</li>
                         </ul>
                     </div>
                 </div>
--- src/Routes/MainRoute/MainRoute.js
+++ src/Routes/MainRoute/MainRoute.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter  } from "react-router-dom"
 import DashboardLayout from "../../Layouts/DashboardLayout/DashboardLayout";
 import MainLayout from "../../Layouts/MainLayout/MainLayout"
+import AdminDashboard from "../../Pages/DashboardPage/AdminDashboard/AdminDashboard";
 import DashboardPanel from "../../Pages/DashboardPage/DashboardPanel/DashboardPanel";
 import HomePageLayout from "../../Pages/HomePage/HomePageLayout/HomePageLayout";
 import ErrorPage from "../../Pages/Shared/ErrorPage/ErrorPage";
@@ -14,7 +15,15 @@ const mainRoute = createBrowserRouter([
             {
                 path : '/',
                 element : <HomePageLayout></HomePageLayout>
-            }
+            },
+            {
+                path : '/blog',
+                element : <HomePageLayout></HomePageLayout>
+            },
+            {
+                path : '/login',
+                element : <HomePageLayout></HomePageLayout>
+            },
         ]
     },
     {
@@ -25,7 +34,11 @@ const mainRoute = createBrowserRouter([
             {
                 path : '/dashboard',
                 element : <DashboardPanel></DashboardPanel>
-            }
+            },
+            {
+                path : '/dashboard/admin',
+                element : <AdminDashboard></AdminDashboard>
+            },
         ]
     }
 ])
