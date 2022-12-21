cc2323674ad04b7bf552225ad5ec590b43b202b1
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-12-22 01:32:43 +0600

    added spinner and fixed blog section, set mobile view issue for category section

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -2,32 +2,36 @@ import React, { useEffect, useState } from 'react';
 import useTitle from '../../Hook/useTitle';
 import axios from 'axios';
 import BlogCard from './BlogCard';
-
-
+import SpinnerPrimary from '../../components/Spinner/SpinnerPrimary'
 
 const Blog = () => {
+  useTitle("Blog Page")
   const [blogsArray, setBlogsArray] = useState([])
-
+  const [loading, setLoading] = useState(false)
   useEffect(() => {
+    setLoading(true)
     axios.get('https://time-craft-server-side.vercel.app/blog')
       .then(function (response) {
         const blogs = response.data
         // console.log(blogs);
         setBlogsArray(blogs);
+        setLoading(false)
       })
       .catch(function (error) {
         // handle error
         console.log(error);
       })
       .finally(function (response) {
-        
+
       });
   }, [])
+  if (loading) {
+    return <SpinnerPrimary />
+  }
 
-  useTitle("Blog Page")
   return (
     <div className='container mx-auto'>
-      <div className='flex flex-col justify-center mx-2 md:mx-0 my-6'>
+      <div className='flex flex-col justify-center mx-2 md:mx-0 my-6 min-h-screen'>
         <h2 className='text-center text-3xl md:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-warning to-primary py-6 my-6 md:mt-8'>
           Blog Page
         </h2>
--- src/Pages/Blog/BlogCard.js
+++ src/Pages/Blog/BlogCard.js
@@ -3,11 +3,17 @@ import React from 'react';
 const BlogCard = ({ blog }) => {
     const { question, imgURL, answer } = blog
     return (
-        <div className="card lg:card-side bg-base-100 shadow-xl">
-            <figure><img src={imgURL} alt="blogs" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">{question}</h2>
-                <p>{answer}</p>
+        <div className="max-w-auto p-4 shadow-md">
+            <div className="space-y-4">
+                <div className="space-y-2">
+                    <img src={imgURL} alt={answer} className="block object-cover object-center w-full rounded-md h-72" />
+                </div>
+                <div className="space-y-2">
+                    <div className="block">
+                        <h3 className="text-xl font-semibold">{question}</h3>
+                    </div>
+                    <p className="leading-snug">{answer}</p>
+                </div>
             </div>
         </div>
     );
